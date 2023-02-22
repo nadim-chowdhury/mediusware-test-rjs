33cf39785bba0bb2de7594d7aa8abf1a03ea36ca
Nadim Chowdhury <40499378+nadim-chowdhury@users.noreply.github.com>
2023-02-22 06:01:35 +0600

    Add files via upload

---
+++ src/components/Problem-2.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+
+const Problem2 = () => {
+  const [contacts, setContacts] = useState([]);
+
+  let contact = contacts.results;
+
+  let API = "https://contact.mediusware.com/api/contacts/?page=1";
+
+  const fetchApiData = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setContacts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchApiData(API);
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
+        <div className="d-flex justify-content-center gap-3">
+          <button className="btn btn-lg btn-outline-primary" type="button">
+            All Contacts
+          </button>
+          <button className="btn btn-lg btn-outline-warning" type="button">
+            US Contacts
+          </button>
+        </div>
+      </div>
+
+      <div id="contacts">
+        {contact.map((ev) => {
+          return <h1>{ev.phone}</h1>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Problem2;
